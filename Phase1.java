86ca6e7cc799e74ae69474470081bcc041b1d5dc
Keith G. Mills <keithgmills34@gmail.com>
2017-03-20 21:28:09 -0600

    This should be a workable, commented, clean version. Unless we need to process more than one file at a time then I think there's a bug.

--- Phase1.java
+++ Phase1.java
@@ -11,6 +11,7 @@ import java.io.*;
 **/
 public class Phase1 {
 
+	// A writer for each file. And the ID of the user.
 	private BufferedWriter termsWriter;
 	private BufferedWriter datesWriter;
 	private BufferedWriter tweetWriter;
@@ -18,12 +19,14 @@ public class Phase1 {
 
 	public static void main(String args[]) {
 
+		// Don't do anything for nothing.
 		if (args.length == 0) {
 			System.out.println("Wrong size of arguments. Exiting.");
 		}
 
 		Phase1 ph = new Phase1();
 
+		// It works on multiple files! 
 		for (String file: args) {
 			ph.parseFile(file);
 		}
@@ -33,16 +36,22 @@ public class Phase1 {
 
 	public void parseFile(String fileName) {
 
+		// Reader for the file. 
 		try(BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
 
+			// Setup writers. 
 			termsWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("terms.txt")));
 			datesWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("dates.txt")));
 			tweetWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("tweets.txt")));
 
+			// Loop for each line, reading entire file.
 			String rawData = reader.readLine();
 			while (rawData != null) {
 
+				// For valid lines.
 				if (rawData.startsWith("<status>")) {
+
+					// Get ID. Send off tweet information. Send off term information. Send off date information. 
 					currentID = getID(rawData);
 					parseTweet(rawData);
 					parseTerms(rawData);
@@ -65,89 +74,53 @@ public class Phase1 {
 		}
 	}
 
+	// Simple enough.
 	public void parseTweet(String line) throws Exception {
 
 		tweetWriter.write(currentID + ":" + line + "\n");
 	}
 
+	// Most complicated of them all. 
 	public void parseTerms(String line) throws Exception {
 
+		// Get the proper fields. Yes, my code is kinda hacky here. 
 		String texts = line.substring(line.indexOf("<text>") + 6, line.indexOf("</text>"));
 		String names = line.substring(line.indexOf("<name>") + 6, line.indexOf("</name>"));
 		String locations = line.substring(line.indexOf("<location>") + 10, line.indexOf("</location>"));
 
+		// For texts, then names, then locations. 
 		matchAndSave(texts, "t");
 		matchAndSave(names, "n");
 		matchAndSave(locations, "l");
 	}
 
+	// Get the date. Then write.
 	public void parseDate(String line) throws Exception {
 
 		String theDate = line.substring(line.indexOf("<created_at>") + 12, line.indexOf("</created_at>"));
 		datesWriter.write(theDate + ":" + currentID + "\n");
 	}
 
+	// Gets ID.
 	public String getID(String line) {
 		return line.substring(line.indexOf("<id>") + 4, line.indexOf("</id>"));
 	}
 
 	public void matchAndSave(String data, String format) throws Exception {
 		
+		// Get rid of all special characters in case they are between two valid terms that should be smooshed together.
 		data = data.replaceAll("&#[0-9]+;", "");
 
+		// Use regex to find the valid terms. 
 		String pattern = "[0-9a-zA-Z_]+";
 		Pattern p = Pattern.compile(pattern);
 		Matcher m = p.matcher(data);
 
-		String toSave[] = {};
+		// Parse the string. Find valid spots. See if they are the proper length, then print in the desired format.
 		while (m.find()) {
-			toSave = processRegexResult(data.substring(m.start(), m.end()));
-			for (String save: toSave) {
-				if (save != null && save.length() > 2) {
-					termsWriter.write(format + "-" + save.replaceAll("&", "").replaceAll("#", "").toLowerCase() + ":" + currentID + "\n");
-				}
+			if (m.end() - m.start() > 2) {
+				termsWriter.write(format + "-" + data.substring(m.start(), m.end()).toLowerCase() + ":" + currentID + "\n");
 			}
 		}
 	}
-
-	public Boolean checkIfShouldBeIgnored(String data, int start, int end) {
-
-		if ((end - start) < 3) {
-			return false;
-		}
-
-		else if ((start - 2) < 0) {
-			return true;
-		}
-
-		else if (data.substring(start - 2, start).equals("&#")) {
-			return false;
-		}
-
-		return true;
-	}
-
-	public String[] processRegexResult(String dataSubString) {
-
-		if (!dataSubString.contains("&") && !dataSubString.contains("#")) {
-			return new String[] {dataSubString};
-		}
-
-		else if (dataSubString.contains("&") && !dataSubString.contains("#")) {
-			return dataSubString.split("&");
-		}
-
-		else if (!dataSubString.contains("&") && dataSubString.contains("#")) {
-			return dataSubString.split("#");
-		}
-
-		String result = "";
-		String chunks[] = dataSubString.split("&#[0-9]+");
-		//return chunks;
-
-		for (String chunk: chunks) {
-			result += chunk;
-		}
-		return new String[] {result};//.replace("#", "").replace("&", "");
-	} 
 }
