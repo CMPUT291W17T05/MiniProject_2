66b5c084a14cf12ec1d8651384b24fc2290ca787
Keith G. Mills <keithgmills34@gmail.com>
2017-03-30 08:52:52 -0600

    Working, but needs a cursor for duplicates

--- QueryProcessor.py
+++ QueryProcessor.py
@@ -21,24 +21,25 @@ def executeSingleQuery(query):
 
 		# Handle all others, aka te.idx
 		if query[0] != 'term':
-			key = ''.join([query[0][0], '-', query[2]])
+			key = ''.join([query[0][0], '-', query[2], '\n'])
 			firstResults = getResults(termsDB, key)
 
 		else:
-			key1 = ''.join(['t', '-', query[2]])
-			key2 = ''.join(['n', '-', query[2]])
-			key3 = ''.join(['l', '-', query[2]])
+			key1 = ''.join(['t', '-', query[2], '\n'])
+			key2 = ''.join(['n', '-', query[2], '\n'])
+			key3 = ''.join(['l', '-', query[2], '\n'])
 			firstResults = firstResults + getResults(termsDB, key1)
 			firstResults = firstResults + getResults(termsDB, key2)
 			firstResults = firstResults + getResults(termsDB, key3)
 
 		termsDB.close()
+		print("FirstResults: ", firstResults)
 
 	tweetDB = db.DB()
 	tweetDB.open('tw.idx', None, db.DB_HASH, db.DB_DIRTY_READ)
 
 	for index in firstResults:
-		finalResults = finalResults + str(tweetDB[index], 'ascii')
+		finalResults.append(str(tweetDB[index.encode()], 'ascii'))
 
 	tweetDB.close()
 
@@ -46,7 +47,10 @@ def executeSingleQuery(query):
 
 def getResults(db, key):
 	if db.has_key(key.encode()):
+		print("Before return ", str(db[key.encode()], 'ascii'))
 		return str(db[key.encode()], 'ascii').split(";")
 
 	else:
 		return []
+
+# Still needs some work and a cursor will be needed.
