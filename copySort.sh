786f61d2eed43c02bd2e74ce86620f5b64bc3329
Keith G. Mills <keithgmills34@gmail.com>
2017-04-02 14:40:12 -0600

    Fixed up the sort

--- copySort.sh
+++ copySort.sh
@@ -1,11 +1,3 @@
-mv tweets.txt tweetsOrig.txt
-mv terms.txt termsOrig.txt
-mv dates.txt datesOrig.txt
-
-sort -u termsOrig.txt -o terms.txt
-sort -u tweetsOrig.txt -o tweets.txt
-sort -u datesOrig.txt -o dates.txt
-
-rm tweetsOrig.txt
-rm termsOrig.txt
-rm datesOrig.txt
+sort -u terms.txt -o terms.txt
+sort -u tweets.txt -o tweets.txt
+sort -u dates.txt -o dates.txt
